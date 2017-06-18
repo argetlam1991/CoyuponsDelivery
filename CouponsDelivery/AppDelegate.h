cfa1c88ece955a5a79eaae1f25353cbae78a4952
Gu Han <guhan@Gus-MacBook-Pro.local>
2017-06-18 12:23:07 -0700

    Initial Commit

---
+++ CouponsDelivery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CouponsDelivery
+//
+//  Created by Gu Han on 6/18/17.
+//  Copyright © 2017 Gu Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CouponsDelivery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CouponsDelivery
+//
+//  Created by Gu Han on 6/18/17.
+//  Copyright © 2017 Gu Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

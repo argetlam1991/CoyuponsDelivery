e9f3b770f3ff4420d322e5513afdf102160adb43
Gu Han <guhan@Gus-MacBook-Pro.local>
2017-06-18 13:36:51 -0700

    Implement trace user's location function

--- CouponsDelivery/ViewController.m
+++ CouponsDelivery/ViewController.m
@@ -7,22 +7,76 @@
 //
 
 #import "ViewController.h"
+#import "MapKit/MapKit.h"
 
-@interface ViewController ()
-
+@interface ViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) MKPointAnnotation *userAnno;
+@property (nonatomic, assign) BOOL mapIsMoving;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void) viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+  
+  self.mapIsMoving = NO;
+  
+  [self setUpLocationManager];
+
+  [self addUserAnno];
+  [self zoomTheMap];
+
+  
 }
 
+#pragma mark - MapView
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+- (void) mapView:(MKMapView *)mapView regionWillChangeAnimated:(BOOL)animated {
+  self.mapIsMoving = YES;
+}
+
+- (void) mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated {
+  self.mapIsMoving = NO;
+}
+
+- (void) centerMap: (MKPointAnnotation *)centerPoint{
+  [self.mapView setCenterCoordinate:centerPoint.coordinate animated:YES];
+}
+
+- (void) zoomTheMap {
+  MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(self.userAnno.coordinate, 500, 500);
+  MKCoordinateRegion adjustedRegion = [self.mapView regionThatFits:viewRegion];
+  [self.mapView setRegion:adjustedRegion animated:YES];
+}
+
+#pragma mark - LocationManager
+
+- (void) setUpLocationManager {
+  self.locationManager = [[CLLocationManager alloc] init];
+  self.locationManager.delegate = self;
+  self.locationManager.allowsBackgroundLocationUpdates = YES;
+  self.locationManager.pausesLocationUpdatesAutomatically = YES;
+  self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+  self.locationManager.distanceFilter = 1; // meters
+  [self.locationManager requestAlwaysAuthorization];
+  self.mapView.showsUserLocation = YES;
+  [self.locationManager startUpdatingLocation];
+}
+
+- (void) addUserAnno {
+  self.userAnno = [[MKPointAnnotation alloc] init];
+  self.userAnno.coordinate = CLLocationCoordinate2DMake(0.0, 0.0);
+  self.userAnno.title = @"My location!";
+
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+  self.userAnno.coordinate = locations.lastObject.coordinate;
+  if (self.mapIsMoving == NO) {
+    [self centerMap:self.userAnno];
+  }
 }
 
 
